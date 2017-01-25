9fb3eb4bd7945a56ffa72d694e4899533c050083
Roni Laukkarinen <roni@laukkarinen.info>
2017-01-25 14:52:02 +0200

    Add extra shade for single service hero

--- content/themes/dude/template-parts/hero-service.php
+++ content/themes/dude/template-parts/hero-service.php
@@ -12,6 +12,7 @@ $hero_desc = get_post_meta( get_the_id(), '_hero_desc', true ); ?>
 
 <div class="slide slide-hero slide-<?php echo get_post_field( 'post_name' ) ?>" <?php if ( has_post_thumbnail() ) : ?> style="background-image:url('<?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id() ) ); ?>');"<?php endif; ?>>
   <div class="shade"></div>
+  <div class="shade shade-extra"></div>
 
   <div class="container">
 
