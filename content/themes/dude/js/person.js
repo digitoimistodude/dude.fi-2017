1254a72cc287e56e773ffdf5c3d5a45d48216cff
Roni Laukkarinen <roni@laukkarinen.info>
2017-11-16 19:24:14 +0200

    Remove empty pointing aria-describedby in slick carousel

--- content/themes/dude/js/person.js
+++ content/themes/dude/js/person.js
@@ -1,2 +1,2 @@
-/*! dude 16-11-2017 17:55 - Digitoimisto Dude Oy (moro@dude.fi) */
+/*! dude 16-11-2017 19:26 - Digitoimisto Dude Oy (moro@dude.fi) */
 !function(t){t(document).ready(function(){t(".number:not(.timefrom) .time span").each(function(){var m=moment(t(this).data("time")).fromNow();t(this).html(m)}),t(".number.timefrom .value").each(function(){var m=moment(t(this).data("time")).fromNow(!0);m=m.split(/(\s+)/),t(this).html(m[0]),t(this).parent().find(".label span").html(m[2]+" ")})})}(jQuery);
--- gulpfile.js
+++ gulpfile.js
@@ -153,7 +153,7 @@ gulp.task('blog-wheel-js', function() {
       gulp.src(
         [
           themeDir + '/node_modules/vue/dist/vue.min.js',
-          themeDir + '/node_modules/slick-carousel/slick/slick.js',
+          themeDir + '/js/src/slick-a11yfix.js',
           themeDir + '/js/src/blog-scroller.js'
         ])
         .pipe(concat('blog-scroller.js'))
