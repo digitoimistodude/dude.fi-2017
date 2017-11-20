eda7df5c447e9888e20da878832f53ba4eb92ae7
Timi Wahalahti <timi.wahalahti@gmail.com>
2017-11-20 13:53:57 +0200

    fix rest likes

--- content/themes/dude/inc/rest-likes.php
+++ content/themes/dude/inc/rest-likes.php
@@ -15,21 +15,23 @@ add_action( 'rest_api_init', function () {
 function dude_like_callback( $request ) {
   $parameters = $request->get_url_params();
 
-  $like_count = get_post_meta( $parameters[1], '_post_like_count', true );
+  $like_count = get_post_meta( $parameters['page'], '_post_like_count', true );
+  $like_count = intval( $like_count );
   $like_count++;
 
-  update_post_meta( $parameters[1], '_post_like_count', $like_count );
+  update_post_meta( $parameters['page'], '_post_like_count', $like_count );
 
-  return array( 'status' => 'success', 'count' => $like_count );
+  return array( 'status' => 'success', 'count' => $like_count, 'post_id' => $parameters['page'] );
 }
 
 function dude_unlike_callback( $request ) {
   $parameters = $request->get_url_params();
 
-  $like_count = get_post_meta( $parameters[1], '_post_like_count', true );
+  $like_count = get_post_meta( $parameters['page'], '_post_like_count', true );
+  $like_count = intval( $like_count );
   $like_count--;
 
-  update_post_meta( $parameters[1], '_post_like_count', $like_count );
+  update_post_meta( $parameters['page'], '_post_like_count', $like_count );
 
-  return array( 'status' => 'success', 'count' => $like_count );
+  return array( 'status' => 'success', 'count' => $like_count, 'post_id' => $parameters['page'] );
 }
