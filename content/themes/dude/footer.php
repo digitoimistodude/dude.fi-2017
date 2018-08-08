7ebabb29d802abd5712f551bbd3606206fa72783
Roni Laukkarinen <roni@laukkarinen.info>
2018-08-08 09:56:24 +0300

    Add Matomo tracking code

--- content/themes/dude/footer.php
+++ content/themes/dude/footer.php
@@ -193,5 +193,21 @@ $coffee = dude_get_coffee_drunk(); ?>
 <script>CRISP_WEBSITE_ID = "-K90vfAnyk27kD-pZAep"</script>
 <script async src="https://client.crisp.im/l.js"></script>
 
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.dude.fi/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
 </body>
 </html>
